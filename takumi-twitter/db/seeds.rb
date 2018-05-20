de853db640a7ed659cb63bf6f21fd061263c3fd5
[metal100] <[takumetal@outlook.jp]>
2018-05-20 12:41:05 +0900

    add first commit

---
+++ takumi-twitter/db/seeds.rb
@@ -0,0 +1,13 @@
+@user = User.new
+@user.name = "Takumi Sato"
+@user.username = "Takumetal"
+@user.location = "Tokyo"
+@user.message = "FUUUUUK!!"
+@user.save
+
+@user = User.new
+@user.name = "Itaru sato"
+@user.username = "Taru"
+@user.location = "Yamagata"
+@user.message = "sit!!"
+@user.save
